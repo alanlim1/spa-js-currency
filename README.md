093a43ecc6e68eefd1fc8d96ed5f81ebb70d0361
Alan <alanoidster@gmail.com>
2018-07-30 16:51:00 +0800

    Create SPA boilerplate for currency rates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Single Page Application
+Project using Vanilla JS to build a SPA without a framework
+
+npm install jquery semantic-ui-css handlebars vanilla-router express dotenv axios
+
+https://www.sitepoint.com/single-page-app-without-framework/
---
+++ public/js/app.js
@@ -0,0 +1,88 @@
+window.addEventListener('load', () => {
+  const el = $('#app');
+
+  // Compile Handlebar Templates
+  const errorTemplate = Handlebars.compile($('#error-template').html());
+  const ratesTemplate = Handlebars.compile($('#rates-template').html());
+  const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
+  const historicalTemplate = Handlebars.compile($('#historical-template').html());
+
+  // Router Declaration
+  const router = new Router({
+    mode: 'history',
+    page404: (path) => {
+      const html = errorTemplate({
+        color: 'yellow',
+        title: 'Error 404 - Page NOT Found!',
+        message: `The path '/${path}' does not exist on this site`,
+      });
+      el.html(html);
+    },
+  });
+
+  // Instantiate api handler
+  const api = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    timeout: 5000,
+  });
+
+  // Display Error Banner
+  const showError = (error) => {
+    const { title, message } = error.response.data;
+    const html = errorTemplate({ color: 'red', title, message });
+    el.html(html);
+  };
+
+  // Display Latest Currency Rates
+  router.add('/', async () => {
+    // Display loader first
+    let html = ratesTemplate();
+    el.html(html);
+    try {
+      // Load Currency Rates
+      const response = await api.get('/rates');
+      const { base, date, rates } = response.data;
+      // Display Rates Table
+      html = ratesTemplate({ base, date, rates });
+      el.html(html);
+    } catch (error) {
+      showError(error);
+    } finally {
+      // Remove loader status
+      $('.loading').removeClass('loading');
+    }
+  });
+
+
+  router.add('/exchange', () => {
+    let html = exchangeTemplate();
+    el.html(html);
+  });
+
+  router.add('/historical', () => {
+    let html = historicalTemplate();
+    el.html(html);
+  });
+
+  // Navigate app to current url
+  router.navigateTo(window.location.pathname);
+
+   // Highlight Active Menu on Refresh/Page Reload
+  const link = $(`a[href$='${window.location.pathname}']`);
+  link.addClass('active');
+
+  $('a').on('click', (event) => {
+    // Block browser page load
+    event.preventDefault();
+
+    // Highlight Active Menu on Click
+    const target = $(event.target);
+    $('.item').removeClass('active');
+    target.addClass('active');
+
+    // Navigate to clicked url
+    const href = target.attr('href');
+    const path = href.substr(href.lastIndexOf('/'));
+    router.navigateTo(path);
+  });
+});
